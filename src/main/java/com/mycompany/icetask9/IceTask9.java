7abad0287a6f17a0936d2f22e0df23ee4b050614
RC_Student_lab <RC_Student_lab@10.0.0.94>
2024-10-16 17:09:58 +0000

    first comment

---
+++ src/main/java/com/mycompany/icetask9/IceTask9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask9;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class IceTask9 {
+
+    public static void main(String[] args) {
+        
+    }
+}
